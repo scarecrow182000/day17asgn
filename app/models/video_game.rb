3962ad3dbda9fd0e80e705b53e6ff27d040dc805
Eric Epperson <eric_d_epperson@hotmail.com>
2014-07-02 09:50:27 -0500

    method picture fix

--- app/models/video_game.rb
+++ app/models/video_game.rb
@@ -3,6 +3,6 @@ class VideoGame < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 2 }
   validates :console, presence: true, length: { minimum: 3 }
   validates :year, presence: true, length: {minimum: 4 }
-  validates :Picture, presence: true
+  validates :picture, presence: true
 
 end
