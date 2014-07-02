9204371336c1fe4d37453bb2d2120ebe475bc794
Eric Epperson <eric_d_epperson@hotmail.com>
2014-07-01 22:54:55 -0500

    Following migration

--- app/models/video_game.rb
+++ app/models/video_game.rb
@@ -1,2 +1,7 @@
 class VideoGame < ActiveRecord::Base
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :console, presence: true, length: { minimum: 3 }
+  validates :year, presence: true, {message: "YYYY" }
+
 end
